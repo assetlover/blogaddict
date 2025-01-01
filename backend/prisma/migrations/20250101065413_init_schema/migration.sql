a350cd501e0db996dc9849c88f2b46b57edda209
assetlover <adityakoshti1234@gmail.com>
2025-01-01 12:39:42 +0530

    db init

---
+++ backend/prisma/migrations/20250101065413_init_schema/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "authorId" TEXT,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,21 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/app/v1/signup", (c) => {
+  return c.text("");
+});
 
-export default app
+app.post("/app/v1/signin", (c) => {
+  return c.text("");
+});
+app.post("/app/v1/blog", (c) => {
+  return c.text("");
+});
+app.put("/app/v1/blog", (c) => {
+  return c.text("");
+});
+app.get("/app/v1/blog:id", (c) => {
+  return c.text("");
+});
+export default app;
